163e21f6184c3a2f332be8d500da7b4b6c8caafe
alfonso96346 <96346@sistemas.frc.utn.edu.ar>
2026-02-02 17:59:42 -0300

    cambio tipografia

---
+++ src/components/SnorlaxCorner.jsx
@@ -0,0 +1,7 @@
+const SnorlaxCorner = () => (
+  <div className="snorlax-corner" aria-hidden="true">
+    <img src="/snorlax.gif" alt="Snorlax saludando" loading="lazy" />
+  </div>
+);
+
+export default SnorlaxCorner;
